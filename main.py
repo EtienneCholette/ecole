8a906beed6aa0c67e8187143edd681e68972bd87
Etienne_Cholette <etichoe@gmail.com>
2023-12-04 20:23:59 -0500

    Le bon pour l'école

--- main.py
+++ main.py
@@ -1,110 +1,166 @@
-# Separate Target Variable and Predictor Variables
-TargetVariable = ['Survived']
-Predictors = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare',
-              'Embarked_C', 'Embarked_Q', 'Embarked_S']
+import numpy as np
+from numpy import array, random, dot, tanh
 
-X = TitanicSurvivalDataNumeric[Predictors].values
-y = TitanicSurvivalDataNumeric[TargetVariable].values
+monday = input("Who came Monday? \n Do Ludovic, Jonathan, Étienne \nEnter three space-separated values: ")
+monday_Matrix = np.array(list(map(int, monday.split())))
 
-### Sandardization of data ###
-### We does not standardize the Target variable for classification
-from sklearn.preprocessing import StandardScaler
+Tuesday = input("Who came Tuesday? \n Do Ludovic, Jonathan, Étienne \nEnter three space-separated values: ")
+Tuesday_Matrix = np.array(list(map(int, Tuesday.split())))
 
-PredictorScaler = StandardScaler()
+Wednesday = input("Who came Wednesday? \n Do Ludovic, Jonathan, Étienne \nEnter three space-separated values: ")
+Wednesday_Matrix = np.array(list(map(int, Wednesday.split())))
 
-# Storing the fit object for later reference
-PredictorScalerFit = PredictorScaler.fit(X)
+Thursday = input("Who came Thursday \n Do Ludovic, Jonathan, Étienne,  \nEnter three space-separated values: ")
+Thursday_Matrix = np.array(list(map(int, Thursday.split())))
 
-# Generating the standardized values of X and y
-X = PredictorScalerFit.transform(X)
+Vincent_action = input("When did Vincent come to class? \nEnter four space-separated values: ")
+Vincent_Matrix = np.array(list(map(int, Vincent_action.split())))
 
-# Split the data into training and testing set
-from sklearn.model_selection import train_test_split
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-
-# Quick sanity check with the shapes of Training and Testing datasets
-print(X_train.shape)
-print(y_train.shape)
-print(X_test.shape)
-print(y_test.shape)
-
-classifier = Sequential()
-# Defining the Input layer and FIRST hidden layer,both are same!
-# relu means Rectifier linear unit function
-classifier.add(Dense(units=10, input_dim=9, kernel_initializer='uniform', activation='relu'))
-
-# Defining the SECOND hidden layer, here we have not defined input because it is
-# second layer and it will get input as the output of first hidden layer
-classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
-
-# Defining the Output layer
-# sigmoid means sigmoid activation function
-# for Multiclass classification the activation ='softmax'
-# And output_dim will be equal to the number of factor levels
-classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
-
-# Optimizer== the algorithm of SGG to keep updating weights
-# loss== the loss function to measure the accuracy
-# metrics== the way we will compare the accuracy after each step of SGD
-classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-# fitting the Neural Network on the training data
-survivalANN_Model = classifier.fit(X_train, y_train, batch_size=10, epochs=10, verbose=1)
-
-# fitting the Neural Network on the training data
-survivalANN_Model = classifier.fit(X_train, y_train, batch_size=10, epochs=10, verbose=1)
+print('We need to determine if Vincent will go to its class')
+user_input = input("Enter three space-separated values: ")
+w = np.array(list(map(int, user_input.split())))
 
 
-# Defining a function for finding best hyperparameters
-def FunctionFindBestParams(X_train, y_train):
-    # Defining the list of hyper parameters to try
-    TrialNumber = 0
-    batch_size_list = [5, 10, 15, 20]
-    epoch_list = [5, 10, 50, 100]
+class NeuralNetwork:
 
-    import pandas as pd
-    SearchResultsData = pd.DataFrame(columns=['TrialNumber', 'Parameters', 'Accuracy'])
+    def __init__(self):
+        random.seed(1)
+        self.weight_matrix = 2 * random.random((3, 1)) - 1
 
-    for batch_size_trial in batch_size_list:
-        for epochs_trial in epoch_list:
-            TrialNumber += 1
+    def tanh(self, x):
+        return tanh(x)
 
-            # Creating the classifier ANN model
-            classifier = Sequential()
-            classifier.add(Dense(units=10, input_dim=9, kernel_initializer='uniform', activation='relu'))
-            classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
-            classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
-            classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    # derivative of tanh function.
+    # Needed to calculate the gradients.
+    def tanh_derivative(self, x):
+        return 1.0 - tanh(x) ** 2
 
-            survivalANN_Model = classifier.fit(X_train, y_train, batch_size=batch_size_trial, epochs=epochs_trial,
-                                               verbose=0)
-            # Fetching the accuracy of the training
-            Accuracy = survivalANN_Model.history['accuracy'][-1]
+    # forward propagation
+    def forward_propagation(self, inputs):
+        return self.tanh(dot(inputs, self.weight_matrix))
 
-            # printing the results of the current iteration
-            print(TrialNumber, 'Parameters:', 'batch_size:', batch_size_trial, '-', 'epochs:', epochs_trial,
-                  'Accuracy:', Accuracy)
+    # training the neural network.
+    def train(self, train_inputs, train_outputs,
+              num_train_iterations):
+        # Number of iterations we want to
+        # perform for this set of input.
+        for iteration in range(num_train_iterations):
+            output = self.forward_propagation(train_inputs)
 
-            SearchResultsData = SearchResultsData.append(pd.DataFrame(data=[[TrialNumber,
-                                                                             'batch_size' + str(
-                                                                                 batch_size_trial) + '-' + 'epoch' + str(
-                                                                                 epochs_trial), Accuracy]],
-                                                                      columns=['TrialNumber', 'Parameters',
-                                                                               'Accuracy']))
-    return (SearchResultsData)
+            # Calculate the error in the output.
+            error = train_outputs - output
+
+            # multiply the error by input and then
+            # by gradient of tanh function to calculate
+            # the adjustment needs to be made in weights
+            adjustment = dot(train_inputs.T, error *
+                             self.tanh_derivative(output))
+
+            # Adjust the weight matrix
+            self.weight_matrix += adjustment
 
 
-###############################################
+# Driver Code
+if __name__ == "__main__":
+    neural_network = NeuralNetwork()
 
-# Calling the function
-ResultsData = FunctionFindBestParams(X_train, y_train)
+    print('Random weights at the start of training')
+    print(neural_network.weight_matrix)
 
-# Printing the best parameter
-print(ResultsData.sort_values(by='Accuracy', ascending=False).head(1))
+    train_inputs = array([monday_Matrix, Tuesday_Matrix, Wednesday_Matrix, Thursday_Matrix])
+    train_outputs = array([Vincent_Matrix]).T
 
-# Visualizing the results
-% matplotlib
-inline
-ResultsData.plot(x='Parameters', y='Accuracy', figsize=(15, 4), kind='line', rot=20)
-classifier.fit(X_train,y_train, batch_size=5 , epochs=100, verbose=1)
+    neural_network.train(train_inputs, train_outputs, 10000)
+
+    print('New weights after training')
+    print(neural_network.weight_matrix)
+
+    # Test the neural network with a new situation.
+    print("Testing network on new examples ->")
+    print(neural_network.forward_propagation(array(w)))
+
+import turtle
+
+
+def draw_circle(x, y, radius, text):
+    turtle.speed(0)
+    turtle.penup()
+    turtle.goto(x, y - radius)
+    turtle.pendown()
+    turtle.circle(radius)
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
+    turtle.write(text, align="center", font=("Arial", 8, "normal"))
+
+
+def draw_line(start, end):
+    turtle.speed(0)
+    turtle.penup()
+    turtle.goto(start)
+    turtle.pendown()
+    turtle.goto(end)
+
+
+def draw_perceptron():
+    turtle.speed(0)
+
+    # Draw input circles
+    draw_circle(-150, 150, 20, 'Ludovic')
+    draw_circle(-150, 50, 20, 'Jonathan')
+    draw_circle(-150, -50, 20, 'Étienne')
+
+    row_index = 0
+    col_index = 0
+    w1 = neural_network.weight_matrix[row_index, col_index]
+
+    row_index = 1
+    col_index = 0
+    w2 = neural_network.weight_matrix[row_index, col_index]
+
+    row_index = 2
+    col_index = 0
+    w3 = neural_network.weight_matrix[row_index, col_index]
+
+    draw_circle(-30, 100, 20, w1)
+    draw_circle(-30, 50, 20, w2)
+    draw_circle(-30, 0, 20, w3)
+
+    # Draw sum circle
+    draw_circle(80, 50, 20, 'Sum')
+
+    # Draw sigma circle (activation)
+    draw_circle(160, 50, 20, 'Σ')
+
+    # Draw output circle
+    draw_circle(260, 50, 20, neural_network.forward_propagation(array(w)))
+
+    # Connect circles with lines
+    draw_line((-130, 150), (-50, 100))
+    draw_line((-130, 50), (-50, 50))
+    draw_line((-130, -50), (-50, 0))
+
+    draw_line((-10, 100), (60, 50))
+    draw_line((-10, 50), (60, 50))
+    draw_line((-10, 0), (60, 50))
+
+    draw_line((100, 50), (140, 50))
+    draw_line((180, 50), (240, 50))
+    turtle.penup()
+
+    turtle.goto(0, -100)  # Move the turtle to (-100, -100)
+    turtle.pendown()
+
+    if (w1 > 1 and w2 > 1) or (w1 > 1 and w3 > 1) or (w2 > 1 and w3 > 1):
+        turtle.write('Not enough data', align="center", font=("Arial", 15, "bold"))
+    else:
+        if neural_network.forward_propagation(array(w)) > 0.9:
+            turtle.write('Vincent will go to the class', align="center", font=("Arial", 15, "bold"))
+        else:
+            turtle.write('Vincent will not go to the class', align="center", font=("Arial", 15, "bold"))
+
+    turtle.done()
+
+
+if __name__ == "__main__":
+    draw_perceptron()
